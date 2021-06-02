1352e66be996a18215bebf487844361f2b4f64bb
Juan Cely <juanscelyg@gmail.com>
2021-06-02 19:57:51 +0200

    Saturators have been configurated

--- scripts/motors_control.py
+++ scripts/motors_control.py
@@ -14,11 +14,15 @@ class GRMI_motors():
         # ROS TIMER
         self.mytime = 0.35
 
+        # Saturator
+        self.vel_saturation = 0.2
+        self.ang_saturation = 1.2
+
         # MODEL
         self.l = 0.15
         self.r = 0.035
         self.jac_inv = np.matrix([[1/self.r, self.l/(self.r)],
-        [1/self.r, self.l/(self.r)]]);
+        [1/self.r, -self.l/(self.r)]]);
         self.speed = 0.0
         self.omega = 0.0
         self.vel_motors = np.zeros(shape=(2,1))
@@ -35,8 +39,14 @@ class GRMI_motors():
 
 
     def callback(self, msg_vel):
-        self.speed = msg_vel.linear.x
-        self.omega = msg_vel.angular.z
+        if abs(msg_vel.linear.x) > self.vel_saturation:
+            self.speed = self.vel_saturation * msg_vel.linear.x / abs(msg_vel.linear.x)
+        else:
+            self.speed = msg_vel.linear.x
+        if abs(msg_vel.angular.z) > self.ang_saturation:
+            self.omega = self.ang_saturation * msg_vel.angular.z / abs(msg_vel.angular.z)
+        else:
+            self.omega = msg_vel.angular.z
         vel = np.matrix([[self.speed],[self.omega]])
         self.vel_motors = np.matmul(self.jac_inv, vel)
 
